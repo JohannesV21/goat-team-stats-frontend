e9b8c4ff72f329039658935f4177db57398483cf
Johannes Villasana <Johannes9969@gmail.com>
2023-10-12 16:40:42 -0400

    feat: home view layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import UserLayout from "@/layouts/UserLayout.component";
-import { Text } from "@chakra-ui/react";
+import { Grid, GridItem, Box, useColorMode } from "@chakra-ui/react";
 import Head from "next/head";
 
 export default function Home() {
+  const { colorMode } = useColorMode();
+  const bgGridItems = colorMode === "light" ? "#FFFFFF" : "RGBA(0, 0, 0, 0.30)";
   return (
     <>
       <Head>
@@ -17,7 +19,77 @@ export default function Home() {
 
       <main>
         <UserLayout>
-          <Text fontSize="32px">HOME</Text>
+          <Box
+            w={{ xl: "95%" }}
+            m={{ base: "20px auto 0 ", md: "20px auto 0" }}
+          >
+            <Grid
+              templateColumns={{ base: "1fr", xl: "repeat(9, 1fr)" }}
+              templateRows={{
+                base: "70vh repeat(3, minmax(350px, 1fr))",
+                xl: "repeat(2, 1fr)",
+              }}
+              gap="20px"
+              h={{ base: "100%", xl: "86vh" }}
+            >
+              <GridItem
+                gridRow={{ xl: "1 / 3" }}
+                gridColumn={{ xl: "1 / 3" }}
+                borderRadius="6px"
+                bgColor={bgGridItems}
+                w={{ base: "95%", xl: "100%" }}
+                m="0 auto"
+                h={"100%"}
+              >
+                <Box w={"100%"} h={"100%"}>
+                  red
+                </Box>
+              </GridItem>
+              <GridItem
+                borderRadius={{ base: "0 0 10px 10px", md: "6px" }}
+                gridRow={{ base: "1 / 2" }}
+                gridColumn={{ xl: "3 / 8" }}
+                bgImage="/images/bannerGoat.webp"
+                bgSize="cover"
+                bgPosition="center"
+                bgRepeat="no-repeat"
+                w={{ base: "100%", md: "95%", xl: "100%" }}
+                h={"100%"}
+                m="0 auto"
+              >
+                <Box w={"100%"} h={"100%"}>
+                  red
+                </Box>
+              </GridItem>
+              <GridItem
+                borderRadius="6px"
+                gridRow={{ xl: "2 / 3" }}
+                gridColumn={{ xl: "3 / 8" }}
+                bgColor={bgGridItems}
+                w={{ base: "95%", xl: "100%" }}
+                m="0 auto"
+                h={"100%"}
+              >
+                <Box w={"100%"} h={"100%"}>
+                  red
+                </Box>
+              </GridItem>
+
+              <GridItem
+                borderRadius="6px"
+                gridRow={{ xl: "1 / 3" }}
+                gridColumn={{ xl: "8 / 10" }}
+                bgColor={bgGridItems}
+                w={{ base: "95%", xl: "100%" }}
+                h={"100%"}
+                m="0 auto"
+              >
+                <Box w={"100%"} h={"100%"}>
+                  red
+                </Box>
+              </GridItem>
+            </Grid>
+          </Box>
         </UserLayout>
       </main>
     </>
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,4 +1,4 @@
-import { extendTheme, type ThemeConfig } from "@chakra-ui/react";
+import { extendTheme, useColorMode, type ThemeConfig } from "@chakra-ui/react";
 import { Montserrat } from "next/font/google";
 
 //fonts
@@ -23,7 +23,6 @@ export const theme = extendTheme({
 
   colors: {
     palette: {
-      fondo: "#F3F8FF",
       green: "#2DCC70",
       gray: { 100: "#666C78" },
       white: { 100: "#FCF7F2" },
@@ -34,8 +33,8 @@ export const theme = extendTheme({
       "html, body": {
         color: props.colorMode === "dark" ? "#FCF7F2" : "#4A5860",
         // color: props.colorMode === "dark" ? "#FCF7F2" : "#1A202C",
-        // bg: props.colorMode === "dark" ? "#232735" : "#F3F4F7",
-        bg: props.colorMode === "dark" ? "#1A202C" : "#F3F4F7",
+        bg: props.colorMode === "dark" ? "#232735" : "#F3F4F7",
+        // bg: props.colorMode === "dark" ? "#1A202C" : "#F3F4F7",
       },
     }),
   },
